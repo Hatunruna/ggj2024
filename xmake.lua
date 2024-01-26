0bc1044911740fbbbfe48166f8bd63de829516a1
Julien Bernard <julien.bernard@univ-fcomte.fr>
2024-01-26 19:59:42 +0100

    change game name

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,9 @@
-set_project("ggj2024")
+set_project("MidnightMovies")
 set_version("0.1.0")
 
 add_repositories("gf-repo https://github.com/GamedevFramework/xmake-repo")
 
-add_requires("gamedevframework1")
+add_requires("gamedevframework1", "nlohmann_json")
 
 add_rules("mode.debug", "mode.releasedbg", "mode.release")
 
@@ -21,9 +21,9 @@ set_configdir("$(buildir)/config")
 set_configvar("GAME_DATADIR", "$(projectdir)/data/Game")
 add_configfiles("code/config.h.in", {pattern = "@(.-)@"})
 
-target("ggj2024")
+target("MidnightMovies")
     set_kind("binary")
     add_files("code/*.cc")
     add_files("code/bits/*.cc")
     add_includedirs("$(buildir)/config")
-    add_packages("gamedevframework1")
+    add_packages("gamedevframework1", "nlohmann_json")
