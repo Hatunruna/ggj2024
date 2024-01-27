33ef3147c05f378f7be16b90d976bcf5c9ebe740
ahugeat <hugeat.arthur@gmail.com>
2024-01-27 12:08:03 +0100

    Create monster tileset

---
+++ data/raw/montage.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+magick montage -background transparent -tile 4x1 -geometry 1239x2160 joie/* ../MidnightMovies/images/monster-happy-a.png
+magick montage -background transparent -tile 4x1 -geometry 1239x2160 colere/* ../MidnightMovies/images/monster-angry-a.png
