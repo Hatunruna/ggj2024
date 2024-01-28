75b8c459fd98e8be0b9c1354bcb92fb582343923
Julien Bernard <julien.bernard@univ-fcomte.fr>
2024-01-28 11:55:40 +0100

    add an icon in the game over scene

--- code/bits/GameOverScene.cc
+++ code/bits/GameOverScene.cc
@@ -9,13 +9,45 @@ namespace mm {
   : gf::Scene(game.getRenderer().getSize())
   , m_game(game)
   , m_entity(game)
+  , m_retryButton(game.resources.getTexture("icons/rotate-right-solid.png"), gf::Color::Azure, gf::Color::lighter(gf::Color::Azure), gf::Color::darker(gf::Color::Azure))
   {
     setClearColor(gf::Color::Black);
 
     addHudEntity(m_entity);
 
+    m_retryButton.setPosition(WorldSize * gf::vec(0.50f, 0.90f));
+    m_retryButton.setAnchor(gf::Anchor::BottomCenter);
+    m_retryButton.setScale(0.5f);
+    m_retryButton.setCallback([this](){
+      // TODO
+    });
+    m_widgets.addWidget(m_retryButton);
+
     setWorldViewSize(WorldSize);
     setWorldViewCenter(WorldSize * 0.5f);
+  }
+
+  void GameOverScene::doProcessEvent(gf::Event& event) {
+    switch (event.type) {
+    case gf::EventType::MouseMoved:
+      m_widgets.pointTo(m_game.getRenderer().mapPixelToCoords(event.mouseCursor.coords, getWorldView()));
+      break;
+
+    case gf::EventType::MouseButtonPressed:
+      m_widgets.pointTo(m_game.getRenderer().mapPixelToCoords(event.mouseButton.coords, getWorldView()));
+      m_widgets.triggerAction();
+      break;
+
+    default:
+      break;
+    }
+  }
+
+  void GameOverScene::doRender(gf::RenderTarget& target, const gf::RenderStates &states) {
+    renderWorldEntities(target, states);
+    renderHudEntities(target, states);
+    m_game.getRenderer().setView(getWorldView()); // HACK
+    m_widgets.render(target, states);
   }
 
 }
--- code/bits/GameOverScene.h
+++ code/bits/GameOverScene.h
@@ -2,8 +2,10 @@
 #define MM_GAME_OVER_SCENE_H
 
 #include <gf/Scene.h>
+#include <gf/WidgetContainer.h>
 
 #include "GameOverEntity.h"
+#include "IconWidget.h"
 
 namespace mm {
 
@@ -13,10 +15,16 @@ namespace mm {
   public:
     GameOverScene(GameHub& game);
 
+    void doProcessEvent(gf::Event& event) override;
+    void doRender(gf::RenderTarget& target, const gf::RenderStates &states) override;
+
   private:
     GameHub& m_game;
 
     GameOverEntity m_entity;
+
+    IconWidget m_retryButton;
+    gf::WidgetContainer m_widgets;
   };
 
 }
